87154cce65d90413958853180dc25a5c86afcb9d
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2013-03-28 21:49:39 +0900

    delete unnecessary routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -23,27 +23,27 @@ import com.feth.play.module.pa.user.AuthUser;
 
 public class Application extends Controller {
 
-	public static final String FLASH_MESSAGE_KEY = "message";
-	public static final String FLASH_ERROR_KEY = "error";
-	public static final String USER_ROLE = "user";
-
+  public static final String FLASH_MESSAGE_KEY = "message";
+  public static final String FLASH_ERROR_KEY = "error";
+  public static final String USER_ROLE = "user";
+  
   public static Result index() {
     String msg     = "Hello, nicocale";
     String dateStr = new DateTime().toString();
     return ok(index.render(msg, dateStr));
   }
 
-	public static User getLocalUser(final Session session) {
-		final AuthUser currentAuthUser = PlayAuthenticate.getUser(session);
-		final User localUser = User.findByAuthUserIdentity(currentAuthUser);
-		return localUser;
-	}
+  public static User getLocalUser(final Session session) {
+    final AuthUser currentAuthUser = PlayAuthenticate.getUser(session);
+    final User localUser = User.findByAuthUserIdentity(currentAuthUser);
+    return localUser;
+  }
 
-	@Restrict(@Group(Application.USER_ROLE))
-	public static Result profile() {
-		final User localUser = getLocalUser(session());
-		return ok(profile.render(localUser));
-	}
+  @Restrict(@Group(Application.USER_ROLE))
+  public static Result profile() {
+    final User localUser = getLocalUser(session());
+    return ok(profile.render(localUser));
+  }
 
   public static Result login() {
     String msg = "login in Nicocale with Twitter";
@@ -66,30 +66,30 @@ public class Application extends Controller {
   public static Result oAuthDenied(final String providerKey) {
     com.feth.play.module.pa.controllers.Authenticate.noCache(response());
     flash(FLASH_ERROR_KEY,
-        "You need to accept the OAuth connection in order to use this website!");
+          "You need to accept the OAuth connection in order to use this website!");
     return redirect(routes.Application.index());
   }
 
-	public static Result signup() {
-		return ok(signup.render(MyUsernamePasswordAuthProvider.SIGNUP_FORM));
-	}
+  public static Result signup() {
+    return ok(signup.render(MyUsernamePasswordAuthProvider.SIGNUP_FORM));
+  }
 
-	public static Result doSignup() {
-		com.feth.play.module.pa.controllers.Authenticate.noCache(response());
-		final Form<MySignup> filledForm = MyUsernamePasswordAuthProvider.SIGNUP_FORM
-				.bindFromRequest();
-		if (filledForm.hasErrors()) {
-			// User did not fill everything properly
-			return badRequest(signup.render(filledForm));
-		} else {
-			// Everything was filled
-			// do something with your part of the form before handling the user
-			// signup
-			return UsernamePasswordAuthProvider.handleSignup(ctx());
-		}
-	}
+  public static Result doSignup() {
+    com.feth.play.module.pa.controllers.Authenticate.noCache(response());
+    final Form<MySignup> filledForm = MyUsernamePasswordAuthProvider.SIGNUP_FORM
+      .bindFromRequest();
+    if (filledForm.hasErrors()) {
+      // User did not fill everything properly
+      return badRequest(signup.render(filledForm));
+    } else {
+      // Everything was filled
+      // do something with your part of the form before handling the user
+      // signup
+      return UsernamePasswordAuthProvider.handleSignup(ctx());
+    }
+  }
 
-	public static String formatTimestamp(final long t) {
-		return new SimpleDateFormat("yyyy-dd-MM HH:mm:ss").format(new Date(t));
-	}
+  public static String formatTimestamp(final long t) {
+    return new SimpleDateFormat("yyyy-dd-MM HH:mm:ss").format(new Date(t));
+  }
 }
